7334f00a40a73848f37f3103ebeb634c46772419
川瀬拓実 <coil_the-world_1341398@i.softbank.jp>
2015-10-26 20:07:39 +0000

    ステンシルバッファ

--- htdocs/scripts/script.js
+++ htdocs/scripts/script.js
@@ -21,44 +21,61 @@ onload = function () {
     c.width = 500;
     c.height = 300;
     c.addEventListener('mousemove', mouseMove, true);
-    var ePointSize = document.getElementById('point_size');
-    var eLines = document.getElementById('lines');
-    var eLineStrip = document.getElementById('line_strip');
-    var eLineLoop = document.getElementById('line_loop');
-    var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+    var gl = c.getContext('webgl', { stencil: true }) || c.getContext('experimental-webgl', { stencil: CSSStyleRule });
     var v_shader = create_shader('vs');
     var f_shader = create_shader('fs');
     var prg = create_program(v_shader, f_shader);
     var attLocation = new Array();
     attLocation[0] = gl.getAttribLocation(prg, 'position');
-    attLocation[1] = gl.getAttribLocation(prg, 'color');
+    attLocation[1] = gl.getAttribLocation(prg, 'normal');
+    attLocation[2] = gl.getAttribLocation(prg, 'color');
+    attLocation[3] = gl.getAttribLocation(prg, 'textureCoord');
     var attStride = new Array();
     attStride[0] = 3;
-    attStride[1] = 4;
-    var pointSphere = sphere(16, 16, 2.0);
-    var pPos = create_vbo(pointSphere.p);
-    var pCol = create_vbo(pointSphere.c);
-    var pVBOList = [pPos, pCol];
+    attStride[1] = 3;
+    attStride[2] = 4;
+    attStride[3] = 2;
     var position = [
-        -1.0, -1.0, 0.0,
-        1.0, -1.0, 0.0,
         -1.0, 1.0, 0.0,
-        1.0, 1.0, 0.0
+        1.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0
+    ];
+    var normal = [
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0
     ];
     var color = [
-        1.0, 1.0, 1.0, 1.0,
         1.0, 0.0, 0.0, 1.0,
         0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0
+        0.0, 0.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0
     ];
-    var lPos = create_vbo(position);
-    var lCol = create_vbo(color);
-    var lVBOList = [lPos, lCol];
+    var textureCoord = [
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        1.0, 1.0
+    ];
+    var index = [
+        0, 1, 2,
+        3, 2, 1
+    ];
+    var vPosition = create_vbo(position);
+    var vNormal = create_vbo(normal);
+    var vColor = create_vbo(color);
+    var vTextureCoord = create_vbo(textureCoord);
+    var vVBOList = [vPosition, vNormal, vColor, vTextureCoord];
+    var vIndex = create_ibo(index);
+    set_attribute(vVBOList, attLocation, attStride);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
     var uniLocation = new Array();
     uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix');
-    uniLocation[1] = gl.getUniformLocation(prg, 'pointSize');
-    uniLocation[2] = gl.getUniformLocation(prg, 'texture');
-    uniLocation[3] = gl.getUniformLocation(prg, 'useTexture');
+    uniLocation[1] = gl.getUniformLocation(prg, 'invMatrix');
+    uniLocation[2] = gl.getUniformLocation(prg, 'lightDirection');
+    uniLocation[3] = gl.getUniformLocation(prg, 'texture');
     var m = new matIV();
     var mMatrix = m.identity(m.create());
     var vMatrix = m.identity(m.create());
@@ -67,56 +84,48 @@ onload = function () {
     var mvpMatrix = m.identity(m.create());
     var invMatrix = m.identity(m.create());
     var qMatrix = m.identity(m.create());
+    var lightDirection = [1.0, 1.0, 1.0];
     var count = 0;
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
-    gl.enable(gl.BLEND);
-    gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE);
     var texture = null;
-    texture = create_texture('../images/texture.png');
+    texture = create_texture('texture.png');
     (function () {
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearColor(0.0, 0.7, 0.7, 1.0);
         gl.clearDepth(1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        var rad = (count % 360) * Math.PI / 180;
+        gl.clearStencil(0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
         count++;
+        m.lookAt([0.0, 0.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
+        m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
         var qMatrix = m.identity(m.create());
         q.toMatIV(qt, qMatrix);
-        var camPosition = [0.0, 5.0, 10.0];
-        m.lookAt(camPosition, [0, 0, 0], [0, 1, 0], vMatrix);
         m.multiply(vMatrix, qMatrix, vMatrix);
-        m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
         m.multiply(pMatrix, vMatrix, tmpMatrix);
-        var pointSize = ePointSize.value / 10;
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        set_attribute(pVBOList, attLocation, attStride);
-        m.identity(mMatrix);
-        m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
-        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
-        gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
-        gl.uniform1f(uniLocation[1], pointSize);
-        gl.uniform1i(uniLocation[2], 0);
-        gl.uniform1i(uniLocation[3], true);
-        gl.drawArrays(gl.POINTS, 0, pointSphere.p.length / 3);
-        var lineOption = 0;
-        if (eLines.checked) {
-            lineOption = gl.LINES;
+        gl.uniform1i(uniLocation[3], 0);
+        gl.enable(gl.STENCIL_TEST);
+        gl.stencilFunc(gl.ALWAYS, 1, ~0);
+        gl.stencilOp(gl.KEEP, gl.REPLACE, gl.REPLACE);
+        render([-0.25, 0.25, -0.5]);
+        gl.stencilFunc(gl.ALWAYS, 0, ~0);
+        gl.stencilOp(gl.KEEP, gl.INCR, gl.INCR);
+        render([0.0, 0.0, 0.0]);
+        gl.stencilFunc(gl.EQUAL, 2, ~0);
+        gl.stencilOp(gl.KEEP, gl.KEEP, gl.KEEP);
+        render([0.25, -0.25, 0.5]);
+        function render(tr) {
+            m.identity(mMatrix);
+            m.translate(mMatrix, [tr[0], tr[1], tr[2]], mMatrix);
+            m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+            m.inverse(mMatrix, invMatrix);
+            gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+            gl.uniformMatrix4fv(uniLocation[1], false, invMatrix);
+            gl.uniform3fv(uniLocation[2], lightDirection);
+            gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
         }
-        if (eLineStrip.checked) {
-            lineOption = gl.LINE_STRIP;
-        }
-        if (eLineLoop.checked) {
-            lineOption = gl.LINE_LOOP;
-        }
-        set_attribute(lVBOList, attLocation, attStride);
-        m.identity(mMatrix);
-        m.rotate(mMatrix, Math.PI / 2, [1, 0, 0], mMatrix);
-        m.scale(mMatrix, [3.0, 3.0, 1.0], mMatrix);
-        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
-        gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
-        gl.uniform1i(uniLocation[3], false);
-        gl.drawArrays(lineOption, 0, position.length / 3);
+        gl.disable(gl.STENCIL_TEST);
         gl.flush();
         setTimeout(arguments.callee, 1000 / 30);
     })();
